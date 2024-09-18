196ca4b6a6074f4f3e93b577b645d1fcd1347203
Raman Siliuk <rsiluk@swiftinvention.com>
2024-09-18 10:03:10 +0400

    removed documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-import starlight from "@astrojs/starlight";
 
 export const site = "https://meextension.org/";
 export const meeFoundation = "https://mee.foundation/";
-export const github = "https://github.com/MeeFoundation/browser-extensions";
-export const discord = "https://discord.com/channels/1275848491964436491/1275848492413223025";
-export const sharedConfig = {
-  title: "Mee Extension",
-  logo: {
-    light: "/src/assets/logo-light.svg",
-    dark: "/src/assets/logo-dark.svg",
-    replacesTitle: true,
-  },
-};
+export const github = "https://github.com/MeeFoundation/browser-extensions/";
+export const privateAdvertising = "https://private-advertising.mee.foundation/";
+export const discord =
+  "https://discord.com/channels/1275848491964436491/1275848492413223025";
+export const extensionLink =
+  "https://chromewebstore.google.com/detail/mee-extension-let-the-web/gfcagdccenaomgheadeimkfdfgkdapcf";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -22,59 +18,5 @@ export default defineConfig({
     mode: "standalone",
   }),
   site: site,
-  integrations: [
-    starlight({
-      ...sharedConfig,
-      logo: {
-        light: "/src/assets/logo-light.svg",
-        dark: "/src/assets/logo-dark.svg",
-        replacesTitle: true,
-      },
-      editLink: {
-        baseUrl: "https://github.com/meefoundation/mee-extension-website/edit/main/",
-      },
-      social: {
-        github: github,
-      },
-      customCss: process.env.NO_GRADIENTS ? [] : ["/src/assets/landing.css"],
-      locales: {
-        root: { label: "English", lang: "en" },
-      },
-      sidebar: [
-        {
-          label: "For Users",
-          items: [
-            {
-              label: "Start Here",
-              items: [
-                {
-                  label: "Getting Started",
-                  link: "docs/users/getting-started",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: "For Developers",
-          items: [
-            {
-              label: "Start Here",
-              items: [
-                {
-                  label: "Getting Started",
-                  link: "docs/developers/getting-started",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      components: {
-        Sidebar: "./src/components/MultiSidebar.astro",
-        EditLink: "./src/components/EditLink.astro",
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [tailwind()],
 });
