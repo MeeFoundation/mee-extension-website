5e69ab48bb069c3a6f605bf8b2d12545a9f86786
Raman Siliuk <rsiluk@swiftinvention.com>
2024-10-11 15:14:17 +0400

    added docs page

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,88 @@ export const discord =
 export const extensionLink =
   "https://chromewebstore.google.com/detail/mee-extension-let-the-web/gfcagdccenaomgheadeimkfdfgkdapcf";
 
+export const header = {
+  links: [
+    {
+      name: "About us",
+      link: meeFoundation,
+      target: "_blank",
+      id: "about-us",
+    },
+    {
+      name: "Docs",
+      link: "/docs",
+      id: "docs",
+    },
+  ],
+  rightLinks: [
+    {
+      name: "Join us",
+      link: discord,
+      icon: "discord",
+      target: "_blank",
+      id: "discord",
+    },
+  ],
+  footerLinks: [
+    {
+      link: meeFoundation,
+      name: "About",
+      target: "_blank",
+      id: "about",
+    },
+    {
+      link: "/privacy-policy/",
+      name: "Privacy Policy",
+      target: "_blank",
+      id: "privacy-policy",
+    },
+    {
+      link: "/docs/",
+      name: "Docs",
+      target: "_blank",
+      id: "docs",
+    },
+  ],
+};
+
+export const footer = {
+  links: [
+    {
+      link: "https://x.com/mee_foundation",
+      icon: "twitter-x",
+      target: "_blank",
+    },
+    {
+      link: github,
+      icon: "github",
+      target: "_blank",
+    },
+    {
+      link: discord,
+      icon: "discord",
+      target: "_blank",
+    },
+  ],
+  rightLinks: [
+    {
+      link: meeFoundation,
+      name: "About",
+      target: "_blank",
+    },
+    {
+      link: "/privacy-policy/",
+      name: "Privacy Policy",
+      target: "_blank",
+    },
+    {
+      link: "/docs/",
+      name: "Docs",
+      target: "_blank",
+    },
+  ],
+};
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
