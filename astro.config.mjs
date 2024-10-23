a44bd953ada0df40732045a22e05e8624262fd0b
Raman Siliuk <rsiluk@swiftinvention.com>
2024-10-23 11:59:40 +0400

    changed mee-components version

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,9 +15,9 @@ export const header = {
   logoLink: meeFoundation,
   links: [
     {
-      name: "Products",
-      link: `${meeFoundation}products/`,
-      id: "products",
+      name: "Projects",
+      link: `${meeFoundation}projects/`,
+      id: "projects",
     },
     {
       name: "Business",
@@ -72,9 +72,9 @@ export const footer = {
   ],
   rightLinks: [
     {
-      name: "Products",
-      link: `${meeFoundation}products/`,
-      id: "products",
+      name: "Projects",
+      link: `${meeFoundation}projects/`,
+      id: "projects",
     },
     {
       name: "Business",
