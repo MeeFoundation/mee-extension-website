94999083f910c0e3d0e84cbcfba413fa455b87c8
Raman Siliuk <rsiluk@swiftinvention.com>
2024-11-04 11:45:35 +0400

    discord link changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ export const site = "https://meextension.org/";
 export const meeFoundation = "https://mee.foundation/";
 export const github = "https://github.com/MeeFoundation/browser-extensions/";
 export const privateAdvertising = "https://private-advertising.mee.foundation/";
-export const discord =
-  "https://discord.com/channels/1275848491964436491/1275848492413223025";
+export const discord = "https://discord.gg/V4vC5zsZ";
 export const extensionLink =
   "https://chromewebstore.google.com/detail/mee-extension-let-the-web/gfcagdccenaomgheadeimkfdfgkdapcf";
 
