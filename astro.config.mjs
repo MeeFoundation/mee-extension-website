ff7372f987fd146bd9428bd331b064acb96dae40
Raman Siliuk <rsiluk@swiftinvention.com>
2024-11-15 14:22:32 +0400

    businesses link updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,9 +19,9 @@ export const header = {
       id: "projects",
     },
     {
-      name: "Business",
-      link: `${meeFoundation}for-providers/`,
-      id: "business",
+      name: 'Businesses',
+      link: `${meeFoundation}businesses/`,
+      id: 'businesses',
     },
     {
       name: "About us",
@@ -76,10 +76,9 @@ export const footer = {
       id: "projects",
     },
     {
-      name: "Business",
-
-      link: `${meeFoundation}for-providers/`,
-      id: "business",
+      name: 'Businesses',
+      link: `${meeFoundation}businesses/`,
+      id: 'businesses',
     },
     {
       name: "About us",
