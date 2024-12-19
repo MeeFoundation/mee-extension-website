df09d5b5bed3d5e35831b208ef783493d5bab044
Raman Siliuk <rsiluk@swiftinvention.com>
2024-12-19 16:59:40 +0400

    mee-components updated, astro updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,112 +1,11 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 export const site = "https://meextension.org/";
-export const meeFoundation = "https://mee.foundation/";
-export const github = "https://github.com/MeeFoundation/browser-extensions/";
-export const privateAdvertising = "https://private-advertising.mee.foundation/";
-export const discord = "https://discord.gg/PB8qxrQXup";
-export const extensionLink =
-  "https://chromewebstore.google.com/detail/mee-extension-let-the-web/ggefahgegbnndppllgajkfjkpmgeahhn";
-
-export const header = {
-  logoLink: meeFoundation,
-  links: [
-    {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
-    },
-    {
-      name: "Businesses",
-      link: `${meeFoundation}businesses/`,
-      id: "businesses",
-    },
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-  ],
-  rightLinks: [
-    {
-      name: "Join us",
-      link: discord,
-      icon: "discord",
-      target: "_blank",
-      id: "discord",
-    },
-  ],
-  footerLinks: [
-    {
-      link: "/privacy-policy/",
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-    {
-      link: "/docs/",
-      name: "Docs",
-      id: "docs",
-    },
-  ],
-};
-export const footer = {
-  links: [
-    {
-      link: "https://x.com/mee_foundation",
-      icon: "twitter-x",
-      target: "_blank",
-    },
-    {
-      link: github,
-      icon: "github",
-      target: "_blank",
-    },
-    {
-      link: discord,
-      icon: "discord",
-      target: "_blank",
-    },
-  ],
-  rightLinks: [
-    {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
-    },
-    {
-      name: "Businesses",
-      link: `${meeFoundation}businesses/`,
-      id: "businesses",
-    },
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-    {
-      link: "/privacy-policy/",
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-    {
-      link: "/docs/",
-      name: "Docs",
-    },
-    {
-      link: "/faq/",
-      name: "FAQ",
-    },
-  ],
-};
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
   site: site,
   integrations: [tailwind()],
 });
